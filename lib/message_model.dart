c32c653fe050547c804751e4c2321acf685fb16d
omarAtallah77 <167710606+omarAtallah77@users.noreply.github.com>
2024-10-27 02:31:42 +0300

    Delete lib/message_model.dart

--- lib/message_model.dart
+++
@@ -1,4 +0,0 @@
-class message_model {
-  String name , number , sender_id   ;
-  DateTime time ;
-  message_model({required this.name , required this.number ,required this.sender_id ,required this.time }) ;}
